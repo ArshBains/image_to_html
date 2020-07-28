d0155a0f0d1de575b23d61f09c0bfd90d920c870
ArshBains <56476189+ArshBains@users.noreply.github.com>
2020-07-28 18:13:24 +0530

    v_1.0

---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+image_location = r'D:\path\to\pic.jpeg'     # image path
+html_loc = r'D:\location\to\save\image.html'    # location to store the .html file with its name
+
+im = Image.open(image_location)
+w, h = im.size
+
+top = '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title>Picture</title><link ' \
+      'rel="stylesheet" href="styles.css"><style>*{margin:0;padding:0;}.main{margin:auto;width:'+str(w)+'px;height:' \
+      +str(h)+';background-color: green;}.px{width:1px;height:1px;float:left;}</style></head><body><div class="main"> '
+foot = '</div></body></html>'
+
+with open(html_loc, 'w') as html:
+    html.write(top)
+    for i in range(0, h):
+        for j in range(0, w):
+            col = str(im.getpixel((j,i)))
+            html.write('<div class="px r'+ str(i) +'c'+ str(j) +'" style="background-color:rgb'+col+';" ></div>')
+    html.write(foot)
